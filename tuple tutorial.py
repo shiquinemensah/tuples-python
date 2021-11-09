5a12542b89a417e812d571e4365811b55c692980
shiquine <godchildshiquine@gmail.com>
2021-11-09 04:38:05 -0800

    tuples py

---
+++ tuple tutorial.py
@@ -0,0 +1,8 @@
+coordinates = (4,5),(6,5),(4,3),6)
+# Tuple is information that connot be changed, onece you have assigned the
+# Information you cannot change it with a different set oo code.
+# Tuples are used to create important information that cannot be changed.
+# the different between a tuple and a string is that a string can be changed but qa tuple cannot.
+print (coordinates[0])
+# always use print to start an action or event
+
